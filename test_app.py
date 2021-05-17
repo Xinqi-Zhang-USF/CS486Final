fc71cc7f0a6f9f1124b17bf71d856c26d26bac40
Xinqi-Zhang-USF <54031478+Xinqi-Zhang-USF@users.noreply.github.com>
2021-05-17 00:29:09 -0700

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,7 +1,7 @@
 from app import xinqi_draw
 
 
-def test_lucky_draw():
+def test_xinqi_draw():
     assert 'Oh you are not Xinqi' == xinqi_draw(1)
     assert 'Geez you are exactly Xinqi' == xinqi_draw(2)
     assert 'Please try one more time' == xinqi_draw(3)
