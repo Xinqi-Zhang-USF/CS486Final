d7a3be43dfaef8fc5c0d098f9f5a66e39fa39926
Xinqi-Zhang-USF <54031478+Xinqi-Zhang-USF@users.noreply.github.com>
2021-05-17 01:05:45 -0700

    Update xinqi-draw-features.py

--- xinqi-draw-features.py
+++ xinqi-draw-features.py
@@ -2,7 +2,6 @@ import pytest, requests, os
 
 from pytest_bdd import scenarios, when, then
 
-
 API = os.getenv('API_URL')
 
 scenarios('./features/app.feature', example_converters=dict(number=int, result=str))
@@ -10,16 +9,16 @@ scenarios('./features/app.feature', example_converters=dict(number=int, result=s
 
 @pytest.fixture
 @when('the Xinqi Draw API is queried with <number>')
-def luck_draw_response(number):
+def xinqi_draw_response(number):
     response = requests.get(API + str(number))
     return response
 
 
 @then('the response status code is 200')
-def luck_draw_response_code(luck_draw_response):
-    assert luck_draw_response.status_code == 200
+def xinqi_draw_response_code(xinqi_draw_response):
+    assert xinqi_draw_response.status_code == 200
 
 
 @then('the response shows <result>')
-def luck_draw_response_result(luck_draw_response, result):
-    assert result == luck_draw_response.text
+def xinqi_draw_response_result(xinqi_draw_response, result):
+    assert result == xinqi_draw_response.text
